786dfc047dca6715449fc1c39c82d40c7addd0e9
Replit user <>
2022-12-09 11:14:20 +0000

    StringOperator

--- basics/input/strOper.py
+++ basics/input/strOper.py
@@ -1,11 +1,14 @@
-print("Please enter the number of lives.")
-lives = input()
-print()
-print("Please enter the energy level.")
-entryLevel = input()
-print()
-print("Please enter the shield level.")
-shieldLevel = input()
-print()
-print()
-print("Health has been set.")
+# Read bot data
+print("Please enter number of lives")
+lives = int(input())
+
+print("Please enter energy level")
+energy = int(input())
+
+print("Please enter shield level")
+shield = int(input()) 
+
+# Display bot data
+print(f"Lives:  {'♥' * lives}")
+print(f"Energy: {'♦' * energy}")
+print(f"Shield: {'♦' * shield}")
