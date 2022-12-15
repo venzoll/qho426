6ff31ef8646b926d4de0da480ed16d9c160bd780
Replit user <>
2022-12-15 07:31:36 +0000

    andOperator

---
+++ basics/decisions/andOperator.py
@@ -0,0 +1,9 @@
+print("What did I hear?")
+noise = input()
+print("What did I see?")
+view = input()
+if ((noise == "grrr") and (view == "two red eyes")):
+  print("There is a scary creature. I should get out of here!")
+else:
+  print("I am a little scared but I will continue.")
+  
