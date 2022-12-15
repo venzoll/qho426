269019952fb5807ef41b47700072e4873575c451
Replit user <>
2022-12-15 23:43:19 +0000

    sum100

---
+++ basics/repetitions/whileLoops/sum100.py
@@ -0,0 +1,14 @@
+print("Calculating the sum of the first 100 numbers...")
+total = 0
+number = 0
+print()
+while (number <= 100):
+    total = total + number
+    number = number + 1
+print("...Done! The answer is", total)
+
+
+
+
+
+
